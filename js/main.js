07a8e065945f781b26805d7aaa8dd1502a578004
juhee1115 <yelow1115@gmail.com>
2026-04-08 16:21:10 +0900

    clone_Artgallery_last

--- js/main.js
+++ js/main.js
@@ -7,14 +7,22 @@ window.addEventListener("load", () => {
       clickable: true,
     },
   });
-});
-
-window.addEventListener("load", () => {
-  const bannerSwiper = new Swiper(".swiper-banner", {
+   const bannerSwiper = new Swiper(".swiper-banner", {
     loop: true,
     pagination: {
       el: ".banner-pagination",
       clickable: true,
     },
   });
+
+  // gotop버튼
+  const goTop = this.document.querySelector("#btn"); 
+  //   gotop버튼 클릭시 위로 스크롤
+  goTop.addEventListener("click", () => {
+    this.window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
 });
+
